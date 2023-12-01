e29ead9ee337202879489a55c5713ea7ee4a3384
lewpar <lewis.o.parmenter@gmail.com>
2023-12-02 09:23:19 +1100

    Added mobile support.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+window.onresize = function()
+{
+    // Clear inline styles to fix media query.
+    if(window.innerWidth > 800)
+    {
+        let sidebar = document.getElementById('sidebar');
+        sidebar.style = '';
+    }
+}
+
 async function loadPage(page)
 {
     let response = await fetch(page);
@@ -16,10 +26,11 @@ async function loadPage(page)
 
 function setHeaderIds()
 {
-    var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    let headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-      // Iterate through each header
-      headers.forEach(function(header) {
+    // Iterate through each header
+    headers.forEach(function(header) 
+    {
         // Get the content of the header
         var headerText = header.textContent || header.innerText;
 
@@ -28,5 +39,12 @@ function setHeaderIds()
 
         // Set the generated ID as the header's id attribute
         header.id = headerId;
-      });
+    });
+}
+
+function toggleSidebar()
+{
+    let sidebar = document.getElementById('sidebar');
+    let display = window.getComputedStyle(sidebar, null).getPropertyValue('display');
+    sidebar.style.display = (display == 'none') ? 'flex' : 'none';
 }
